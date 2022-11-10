f35ba8e5d70351d41a33fe5abc1f00a0ab34634e
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-10 15:51:47 -0500

    basic basic basic api

--- express.ts
+++ express.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 
 const app = express();
@@ -6,7 +6,9 @@ app.use(express.json());
 app.use(cors());
 
 
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello typescript")  
+})
 
 app.listen(5005, () => {
   console.log('http://localhost:5005')
